b8b79660d0e76ee1bc1ce370a7a9306fcccfe79e
Aaron Sarazan <aaron@sarazan.net>
2012-08-25 22:30:10 -0700

    jankotron stubs

---
+++ src/com/virtuoussoftware/apptly/auth/Credentials.java
@@ -0,0 +1,5 @@
+package com.virtuoussoftware.apptly.auth;
+
+public class Credentials {
+
+}
---
+++ src/com/virtuoussoftware/apptly/jankotron/SlowTabulizerActivity.java
@@ -0,0 +1,17 @@
+package com.virtuoussoftware.apptly.jankotron;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class SlowTabulizerActivity extends FragmentActivity {
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		
+		// TODO (asarazan) inflate pretty layout
+	}
+	
+	private void fetchStuff() {
+		// TODO (asarazan) creds
+	}
+}
