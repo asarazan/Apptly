d7c286fe627beaaf93eacd9186b900ba0842912f
Aaron Sarazan <aaron@sarazan.net>
2012-08-25 22:33:07 -0700

    credentials

--- src/com/virtuoussoftware/apptly/auth/Credentials.java
+++ src/com/virtuoussoftware/apptly/auth/Credentials.java
@@ -1,5 +1,6 @@
 package com.virtuoussoftware.apptly.auth;
 
 public class Credentials {
-
+	static final String CLIENT_ID = "jShMUKBBhfqAHPYexs4aEnFHNz996JSE";
+	static final String CLIENT_SECRET = "wtQsewJK7PCd77uWK7AYLHm8UyCYa77j";
 }
